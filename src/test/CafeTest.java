7a09243db1cdac25d8dbf3083f287994e874ddcd
olenaivaniuk <oivaniuk.scout24@gmail.com>
2019-09-06 16:49:38 +0200

    First unit tests

--- src/test/CafeTest.java
+++ src/test/CafeTest.java
@@ -9,17 +9,38 @@ public class CafeTest {
 
     @Test
     public void canBrewEspressso () {
+        //given
         Cafe cafe = new Cafe();
         cafe.restockBeans(7);
+
+        //when
         Coffee coffee = cafe.brew(CoffeeType.Espresso);
 
-        // it is espresso;
-        // no milk
-        // that we got enought coffee
+        //then
         Assert.assertEquals(CoffeeType.Espresso, coffee.getType());
         Assert.assertEquals(0, coffee.getMilk());
         Assert.assertEquals(7, coffee.getBeans());
+    }
 
+    @Test
+    public void brewingEspressoConsumesBeans() {
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(7);
+
+        //when
+        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+
+        //then
+        Assert.assertEquals(0, cafe.getBeansInStock());
+    }
+
+    @Test
+    (expected = IllegalStateException.class)
+    public void latteIsRequireMilk() {
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(7);
+
+        cafe.brew(CoffeeType.Latte);
 
     }
 
